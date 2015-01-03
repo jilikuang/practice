2712550b025b3293b533187f2dc1a4b6bb0076a5
Jie-Gang Kuang <jk3735@columbia.edu>
2015-01-03 02:09:33 -0500

    Add list node random generation

--- linkedlist/ListNode.java
+++ linkedlist/ListNode.java
@@ -1,6 +1,14 @@
+import java.util.Random;
+
 public class ListNode {
 	int val;
 	ListNode next;
+
+	public ListNode() {
+		Random r = new Random();
+		val = r.nextInt();
+		next = null;
+	}
 
 	public ListNode(int x) {
 		val = x;
