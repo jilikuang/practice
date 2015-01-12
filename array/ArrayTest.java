a3604312daef88a2814400be859bc1dcab23aa63
Jie-Gang Kuang <jk3735@columbia.edu>
2015-01-12 16:15:31 -0500

    Add selection sorting algorithm

--- array/ArrayTest.java
+++ array/ArrayTest.java
@@ -4,7 +4,7 @@ public class ArrayTest {
 		int[] array = ArrayUtils.randomArray(20, 100);
 
 		ArrayUtils.printArray(array);
-		ArraySort.insertSort(array);
+		ArraySort.selectSort(array);
 		ArrayUtils.printArray(array);
 	}
 }
