1f98fc6fd2a3114817688468720c2070438c7956
Jie-Gang Kuang <jk3735@columbia.edu>
2015-01-25 15:08:32 -0500

    Move out unique character check

--- string/unique_char.c
+++ string/unique_char.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char **argv) {
+static int unique_characters(char *str) {
 	unsigned int bitmaps[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-	int i, l;
-
-	if (argc < 2) {
-		printf("Need a string input\n");
-		return -1;
-	}
-
-	l = strlen(argv[1]);
+	int i, l = strlen(str);
+	int c, d;
 
 	for (i = 0; i < l; i++) {
-		int c = (int)argv[1][i];
-		int d = 0;
+#if 0
+		c = (int)str[i];
+		d = 0;
 
 		while (c >= 0) {
 			if (c < 32)
@@ -22,6 +17,11 @@ int main(int argc, char **argv) {
 			d++;
 			c -= 32;
 		}
+#else
+		c = (int)str[i];
+		d = c / 32;
+		c = c % 32;
+#endif
 
 		if ((bitmaps[d] >> c) & 0x1)
 			break;
@@ -29,7 +29,18 @@ int main(int argc, char **argv) {
 		bitmaps[d] |= (0x1 << c);
 	}
 
-	if (i == l)
+	return i == l;
+}
+
+int main(int argc, char **argv) {
+
+	if (argc < 2) {
+		printf("Need a string input\n");
+		return -1;
+	}
+
+
+	if (unique_characters(argv[1]))
 		printf("The input has unique characters\n");
 	else
 		printf("The input has duplicate characters\n");
