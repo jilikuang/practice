fca746e095a26eac14946a7cc6df8de3234bc1e1
Jie-Gang Kuang <jk3735@columbia.edu>
2015-01-28 21:20:55 -0500

    Add more tree node attributes

--- binarytree/TreeNode.java
+++ binarytree/TreeNode.java
@@ -2,18 +2,24 @@ import java.util.Random;
 
 public class TreeNode {
 	int val;
+	int size;
+	int height;
 	TreeNode left;
 	TreeNode right;
 
 	public TreeNode() {
 		Random r = new Random();
 		val = r.nextInt();
+		size = 1;
+		height = 0;
 		left = null;
 		right = null;
 	}
 
 	public TreeNode(int x) {
 		val = x;
+		size = 1;
+		height = 0;
 		left = null;
 		right = null;
 	}
