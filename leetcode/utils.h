fdc992c30fda6de8e2d55534da30ba5aa4187187
Jie-Gang Kuang <jk3735@columbia.edu>
2015-08-25 18:26:41 -0400

    Add common utils for tests

---
+++ leetcode/utils.h
@@ -0,0 +1,62 @@
+#ifndef __UTILS__
+#define __UTILS__
+
+#include <stdlib.h>
+#include <stdio.h>
+
+struct ListNode {
+	int val;
+	struct ListNode *next;
+};
+
+/*
+ * Functions
+ */
+
+void list_append(struct ListNode **p_head, int val)
+{
+	if (!p_head) {
+		printf("Invalid list head pointer!\n");
+		return;
+	}
+
+	if (*p_head) {
+		struct ListNode *head = *p_head;
+
+		while (head->next)
+			head = head->next;
+
+		head->next = malloc(sizeof(struct ListNode));
+		head->next->val = val;
+		head->next->next = NULL;
+	} else {
+		*p_head = malloc(sizeof(struct ListNode));
+		(*p_head)->val = val;
+		(*p_head)->next = NULL;
+	}
+}
+
+void list_release(struct ListNode *head)
+{
+	while (head) {
+		struct ListNode *curr = head;
+
+		head = curr->next;
+		free(curr);
+	}
+}
+
+void list_print(struct ListNode *head)
+{
+	printf("[");
+	while (1) {
+		printf("%d", head->val);
+		if (!head->next)
+			break;
+		printf(", ");
+		head = head->next;
+	}
+	printf("]\n");
+}
+
+#endif	// __UTILS__
