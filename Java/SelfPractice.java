ce384d51fb2bc669e6a88f23396ebc6998fbf8a3
Jie-Gang Kuang <jgkuang@gmail.com>
2015-12-10 11:51:43 -0500

    Add .gitignore for Java and Python practices

--- Java/SelfPractice.java
+++ Java/SelfPractice.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+
 public class SelfPractice {
 	public static void main(String[] args) {
 		System.getProperties().list(System.out);
+		System.out.println("\nargs: " + Arrays.toString(args));
+		SelfPractice practice = new SelfPractice();
+		char a = 'a';
+		String abc = "abcdefghijklmnopqrstuvwxyz";
+		String ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		char[] abcCh = abc.toCharArray();
+		char[] ABCCh = ABC.toCharArray();
+
+		System.out.println(Character.getNumericValue(a));
+		System.out.println((int)a);
 	}
 }
--- Python/self_practice.py
+++ Python/self_practice.py
@@ -1,5 +1,17 @@
+class Test:
+
+    def __init__(self, key, value):
+        self.key = key
+        self.val = value
+
 def main():
     print "This is the self practice of Python"
+    a = Test(1, 1)
+    b = Test(2, 2)
+    d = {}
+    d[a] = 'a'
+    d[b] = 'b'
+    print d
 
 if __name__ == "__main__":
     main()
